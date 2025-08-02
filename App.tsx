01be34279b7003e2ef7a4939a62d7c60fc4494b6
Nick055 <nicky.conrad@hotmail.com>
2025-08-02 11:28:48 +0200

    - Renamed "create new sequence" to "edit sequence" - Added ability to save sequences as JSON via AsyncStorage

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {StyleSheet, View, Text, Button, ScrollView, TextInput} from 'react-native';
 import styles from './styles';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,7 +13,7 @@ const HomeScreen = ({navigation}) => {
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>sequences</Text>
       <Button title="Create New Sequence" onPress={()=>{
-        navigation.navigate("New Sequence")
+        navigation.navigate("Edit Sequence")
       }}/>
       <Button title="View Existing Sequences" onPress={()=>{}}/>
     </View>
@@ -20,11 +21,11 @@ const HomeScreen = ({navigation}) => {
 
 }
 
-const NewSequenceScreen = () => {
+const EditSequenceScreen = () => {
 
   const [sequenceName, setSequenceName] = React.useState(() => {
     const dateObj = new Date();
-    let dateStr = dateObj.toLocaleDateString('fr-CA') + "_" + dateObj.toLocaleTimeString('en-GB', {hour12: false});
+    let dateStr = "seq_"+dateObj.toLocaleDateString('fr-CA') + "_" + dateObj.toLocaleTimeString('en-GB', {hour12: false});
     dateStr = dateStr.replace(/:/g, '-');
     return dateStr;
   });
@@ -42,11 +43,11 @@ const NewSequenceScreen = () => {
     <View style={styles.sectionContainer}>
       <Text>Section {sectionsNo+1}</Text>
       <Text>Tempo (bpm):</Text>
-      <TextInput keyboardType="numeric" placeholder="100" onChangeText={text => setTempi([...tempi,Number(text)])}/>
+      <TextInput keyboardType="numeric" placeholder="120" onChangeText={text => setTempi([...tempi,Number(text)])}/>
       <Text>Number of Beats per Bar:</Text>
       <TextInput keyboardType="numeric" placeholder="4" onChangeText={text => setBeatsPerBar([...beatsPerBar,Number(text)])}/>
       <Text>Number of Bars:</Text>
-      <TextInput keyboardType="numeric" placeholder="8" onChangeText={text => setBars([...beatsPerBar,Number(text)])}/>
+      <TextInput keyboardType="numeric" placeholder="8" onChangeText={text => setBars([...bars,Number(text)])}/>
     </View>
     );
     setSectionsNo(sectionsNo + 1);
@@ -59,18 +60,31 @@ const NewSequenceScreen = () => {
     }
   }
 
+  const saveSequence = async () => {
+    if (sectionsNo > 0) {
+      const SequenceObject = {
+        name: sequenceName,
+        tempi: tempi,
+        beatsPerBar: beatsPerBar,
+        bars: bars
+      };
+      const sequenceJSON = JSON.stringify(SequenceObject);
+      await AsyncStorage.setItem(sequenceName, sequenceJSON);
+    }
+  }
+
   return (
-    <ScrollView style={{flex: 1}}>
-      <Text>Create New Sequence</Text>
+    <View style={{flex: 1}}>
+      <Text>Edit Sequence</Text>
       <Text>Sequence Name:</Text>
       <TextInput value={sequenceName} onChangeText={setSequenceName}/>
-      <Button title="Add Tempo Section" onPress={addSection}/>
       <ScrollView>
         {sectionElements}
       </ScrollView>
+      <Button title="Add Tempo Section" onPress={addSection}/>
       <Button title="Remove Last Section" onPress={removeSection}/>
-      <Button title="Save Sequence" onPress={()=>{}}/>
-    </ScrollView>
+      <Button title="Save Sequence" onPress={saveSequence}/>
+    </View>
   );
 
 }
@@ -81,7 +95,7 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="New Sequence" component={NewSequenceScreen} />
+        <Stack.Screen name="Edit Sequence" component={EditSequenceScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- styles.js
+++ styles.js
@@ -3,7 +3,7 @@ import {StyleSheet} from 'react-native';
 export default StyleSheet.create({
     sectionContainer: {
         padding: 20,
-        margin: 20,
+        margin: 10,
         borderWidth: 2,
         borderColor: '#ccc',
         borderRadius: 5
